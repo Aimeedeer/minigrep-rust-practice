9a4bfd64b57b821ced36fd80993fd228dc951cf4
aimeedeer <aimeez@pm.me>
2021-02-23 10:55:48 -0600

    finished ch12

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
+use minigrep::Config;
 use std::env;
 use std::process;
-use minigrep::Config;
 
-// run with
-// $ cargo run <query_text> poem.txt
+// run: 
+// $ cargo run to poem.txt
+// $ CASE_INSENSITIVE=1 cargo run to poem.txt
+// $ cargo run to poem.txt > output.txt
 fn main() {
     let args: Vec<String> = env::args().collect();
+
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for {} in file {}", config.query, config.filename);
     if let Err(e) = minigrep::run(config) {
-        println!("Run(config) error: {}", e);
+        eprintln!("Run(config) error: {}", e);
         process::exit(1);
     }
 }
