dc83be711cddc27f599ec81db688db78c5fedc38
aimeedeer <aimeez@pm.me>
2021-02-26 10:22:54 -0500

    Rust book ch13

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,7 @@ use std::{env, process};
 // $ CASE_INSENSITIVE=1 cargo run to poem.txt
 // $ cargo run to poem.txt > output.txt
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
